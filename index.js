4de184588e31485ec5f0bbb186782ef8a8e0ba3b
Danny Blue <dannybluedesign@gmail.com>
2015-11-23 09:04:10 -0500

    update code style

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
 var fs = require('fs');
 
+module.exports = findAndReplace;
+
 /**
  * @name  Find and Replace
- * @param file the path the file
- * @param dest where to send the file
- * @param values object with the variable to be replaced and its replacement
- * @param callback
+ *
+ * @param {string} file - the path the file
+ * @param {string} dest - where to send the file
+ * @param {object} values - object with the variable to be replaced and its replacement
+ * @param {function} callback
  */
-module.exports = function(file, dest, values, callback)
-{
-
+function findAndReplace(file, dest, values, callback) {
   var regex;
 
-  //Read file
-  fs.readFile(file, function(err, data)
-  {
-    if(err) { return err; }
+  fs.readFile(file, function(err, data) {
+    if(err) throw err;
 
     var replaced = data.toString();
 
-
-    for(var key in values)
-    {
-
-      if(values.hasOwnProperty(key))
-      {
+    for(var key in values) {
+      if(values.hasOwnProperty(key)) {
 
         regex = new RegExp(key, 'g');
 
-        var contains = replaced.indexOf(key);
-
-        if(contains > -1)
-        {
-
+        if(replaced.indexOf(key) > -1) {
           replaced = replaced.replace(regex, values[key]);
-
         }
-        else
-        {
-
-          return console.error('Template Value ' + key + ' is not found');
-
+        else {
+          return console.log('Template Value ' + key + ' is not found');
         }
-
       }
-
     }
 
-    fs.writeFile(dest, replaced, function(err)
-    {
+    fs.writeFile(dest, replaced, function(err) {
+      if(err) throw err;
 
-      if(err) { throw err; }
+      if(callback) callback();
 
-      return callback ? callback(replaced) : true;
-
+      return 1;
     });
-
   });
-
-};
+}
